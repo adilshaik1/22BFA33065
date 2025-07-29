09f453b3c09ebbd0980b0ea98b8be6bbfdd8696f
adilshaik1 <112190768+adilshaik1@users.noreply.github.com>
2025-07-29 11:53:42 +0530

    Update App.tsx

--- Frontend Test Submission/src/App.tsx
+++ Frontend Test Submission/src/App.tsx
@@ -1,1 +1,15 @@
+import { Routes, Route } from 'react-router-dom' ;
+import ShortenPage from './pages/ShortenPage';
+import StatsPage from './pages/StatsPage';
+import RedirectPage from './pages/RedirectPage';
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<ShortenPage />} />
+      <Route path="/stats" element={<StatsPage />} />
+      <Route path="/:shortcode" element={<RedirectPage />} />
+    </Routes>
+  );
+};
 
+export default App;
